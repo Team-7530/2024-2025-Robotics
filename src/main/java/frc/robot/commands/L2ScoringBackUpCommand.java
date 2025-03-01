868b201b99512f6636770f10f1185ebd6c162fc9
aheidorn <allen.heidorn@gmail.com>
2025-03-01 12:50:57 -0600

    Added frame for backup command

---
+++ src/main/java/frc/robot/commands/L2ScoringBackUpCommand.java
@@ -0,0 +1,14 @@
+package frc.robot.commands;
+
+import static frc.robot.Constants.*;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+
+ public class L2ScoringBackUpCommand extends ParallelCommandGroup {
+
+   public L2ScoringBackUpCommand(CommandSwerveDrivetrain drivetrain) {
+    
+   }
+ }
